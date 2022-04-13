7910c1fb97ab3c4b023b4c7570cfedd7a8e43878
RichieLoco <richieloco@hotmail.co.uk>
2022-04-13 16:01:24 +0100

    Test Submit

--- src/main/java/hackerrank/FurnitureOrder.java
+++ src/main/java/hackerrank/FurnitureOrder.java
@@ -4,9 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FurnitureOrder implements FurnitureOrderInterface {
-	/**
-	 * TODO: Create a map of Furniture items to order quantities
-	 */
 
 	private final Map<Furniture, Integer> order;
 
